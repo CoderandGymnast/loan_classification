e1d876c95a165ad731b691cab7bb7cf58df5f67d
Thien <ambitapperubik@gmail.com>
2021-05-15 22:39:36 +0700

    Preprosessing 2

--- main.m
+++ main.m
@@ -1,16 +1,22 @@
-Preprocessing;
+%DataFile = "CreditRating_Historical.dat";
+%Preprocessing(DataFile);
+
+pkg load io
+trainData = load("trainData.dat");
+trainLabels = load("trainLabels.dat");
 
 numNodes = [7 7 7];
-m = size(train_data, 1);
-train_data = [ones(m, 1) train_data];
+m = size(trainData, 1);
+trainData = [ones(m, 1) trainData];
 learningRate = 0.05;
 regularizationRate = 0.02;
 weights1 = rand(numNodes(2)-1, numNodes(1));
 weights2 = rand(numNodes(3), numNodes(2));
 numIters = 100;
 
-[weights1, weights2, histories] = gradientDescent(m, train_data, train_labels, weights1, weights2, learningRate, numIters, regularizationRate);
+[weights1, weights2, histories] = gradientDescent(m, trainData, trainLabels, weights1, weights2, learningRate, numIters, regularizationRate);
 
 saveWeights(weights1, weights2, numNodes);
 
 
+plot(histories);
