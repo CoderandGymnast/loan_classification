5f761360406172fd4b2bf025a2b6ec2aff943688
Sonic <HungICT2407@gmail.com>
2021-05-16 00:40:40 +0700

    trained model

--- gradientDescent.m
+++ gradientDescent.m
@@ -1,16 +1,19 @@
-function [weights1, weights2, histories] = gradientDescent(m, X, Y, weights1, weights2, learningRate, numIters, regularizationRate)
+function [weights1, weights2, learningRate, histories] = gradientDescent(m, X, Y, weights1, weights2, learningRate, numIters, regularizationRate)
 
     histories = [];
     for i = 1:numIters
 
         % I. Gradient descent:
         [weights1Gradients, weights2Gradients, J] = backPropagation(m, X, Y, weights1, weights2, regularizationRate);
+		if((i > 1)&& (histories(i-1) < J))
+			learningRate/=2;
+		end
         weights1 = weights1 - learningRate * weights1Gradients;
         weights2 = weights2 - learningRate * weights2Gradients;
 
         % II. Compute cost:
-  
         histories(i) = J;
     end
+
 
 end
--- main.m
+++ main.m
@@ -1,5 +1,5 @@
-DataFile = "CreditRating_Historical.dat";
-Preprocessing(DataFile);
+%DataFile = "CreditRating_Historical.dat";
+%Preprocessing(DataFile);
 
 pkg load io
 trainData = load("trainData.dat");
@@ -8,15 +8,27 @@ trainLabels = load("trainLabels.dat");
 numNodes = [7 7 7];
 m = size(trainData, 1);
 trainData = [ones(m, 1) trainData];
-learningRate = 0.05;
-regularizationRate = 0.02;
-weights1 = rand(numNodes(2)-1, numNodes(1));
-weights2 = rand(numNodes(3), numNodes(2));
-numIters = 100;
+learningRate = 0.7;
+regularizationRate = 0.001;
+%weights1 = rand(numNodes(2)-1, numNodes(1));
+%weights2 = rand(numNodes(3), numNodes(2));
+pkg load io
+weights1 = cell2mat(csv2cell("weights1.dat"));
+weights2 = cell2mat(csv2cell("weights2.dat"));
 
-[weights1, weights2, histories] = gradientDescent(m, trainData, trainLabels, weights1, weights2, learningRate, numIters, regularizationRate);
+numIters = [1000];
 
-saveWeights(weights1, weights2, numNodes);
+numBatch = 1;
 
+for i = 1:numBatch
+
+    tic
+    [weights1, weights2, learningRate, histories] = gradientDescent(m, trainData, trainLabels, weights1, weights2, learningRate, numIters(i), regularizationRate);
+    toc
+    histories(numIters(i))
+
+end
+
+%saveWeights(weights1, weights2, numNodes);
 
 plot(histories);
